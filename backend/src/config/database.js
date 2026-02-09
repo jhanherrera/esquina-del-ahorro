2054b013dc89fd77a772e70f9bc0f3c5d3f282c7
jhanherrera <jhan12herrera@gmail.com>
2026-02-08 21:35:32 -0500

    contraseña de base de datos

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "admin",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "supermercado",
   port: process.env.DB_PORT || 3306
 });
